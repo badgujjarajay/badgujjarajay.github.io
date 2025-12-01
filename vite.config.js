1e728039b5d65b61091884554d880682aa0d2ad1
Ajay <badgujjarajay@gmail.com>
2025-12-02 03:33:00 +0530

    feat: logo

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Set base to './' to allow deployment to any subdirectory (GitHub Pages project site)
   // or the root (User site). This fixes the blank page issue if paths are wrong.
-  base: '/',
+  base: './',
 })
